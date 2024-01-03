abe4ac4c2b55d88305e8dc3d7c7f2547e0e43bcd
alex13315015 <alex13315015@gmail.com>
2024-01-03 17:40:28 +0900

    first commit

---
+++ src/main/java/com/ryan9025/pjnovel/PjNovelApplication.java
@@ -0,0 +1,13 @@
+package com.ryan9025.pjnovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjNovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjNovelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryan9025/pjnovel/PjNovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan9025.pjnovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjNovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
